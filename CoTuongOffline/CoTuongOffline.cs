b3310bba1ed3cc74cf5c591e980b77817a2414b7
Duyisme <duyisme2003@gmail.com>
2023-06-10 12:34:45 +0700

    Add project files.

---
+++ CoTuongOffline/CoTuongOffline.cs
@@ -0,0 +1,186 @@
+﻿using CoTuongOffline.CoTuong;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace CoTuongOffline
+{
+    public partial class CoTuongOffline : Form
+    {
+        bool sound;
+        System.Media.SoundPlayer player;
+
+        //Camera
+        private static Bitmap screenBitmap;
+        private static Graphics screenGraphics;
+
+        public CoTuongOffline()
+        {
+            InitializeComponent();
+            PlayMusic();
+        }
+
+        public void PlayMusic()
+        {
+            player = new System.Media.SoundPlayer(Properties.Resources.Nhac3);
+            player.PlayLooping();
+        }
+
+        private void CoTuongOffline_Load(object sender, EventArgs e)
+        {
+
+            BanCo.SetToDefault(lblPheDuocDanh, lblSoLuotDi, btnNewGame, btnUndo);
+            BanCo.TaoDiemBanCo(ptbBanCo, DiemBanCo_Click);
+            BanCo.TaoQuanCo(QuanCo_Click, ptbBanCo);
+            BanCo.RefreshBanCo();
+        }
+        /* Khi click vào 1 RoundPictureBox quân cờ thì nó sẽ được chọn... */
+        private void QuanCo_Click(object sender, EventArgs e)
+        {
+            BanCo.QuanCoDuocChon = sender as ProgramConfig.RoundPictureBox;
+            BanCo.Highlight(ptbBanCo);
+            BanCo.HienThiDiemDich();
+            foreach (ProgramConfig.RoundPictureBox element in BanCo.Alive_RoundPictureBox) // Khi 1 quân cờ được chọn thì không thể click chọn 1 quân cờ khác ngay lập tức mà phải bỏ chọn nó trước
+                element.Enabled = false;
+        }
+
+        private void ptbBanCo_Click(object sender, EventArgs e)
+        {
+            if (BanCo.QuanCoDuocChon != null)
+            {
+                BanCo.Dehighlight();
+                BanCo.AnDiemDich();
+                BanCo.RefreshBanCo(); //*Offline*
+                BanCo.QuanCoDuocChon = null;
+            }
+        }
+        /* Những gì xảy ra khi click vào một RoundButton điểm bàn cờ để đi đến */
+        private void DiemBanCo_Click(object sender, EventArgs e) // BẢN OFFLINE
+        {
+            if (BanCo.QuanCoDuocChon == null) return; // Dòng code chống lỗi lặp lại event ngoài ý muốn (chưa rõ nguyên nhân của lỗi này). Không được xóa!
+            BanCo.Dehighlight(); // chọn nước đi...
+            BanCo.AnDiemDich(); // ...thì đồng thời sẽ bỏ chọn quân cờ luôn
+
+            Point departure = new Point(BanCo.QuanCoDuocChon.Quan_Co.ToaDo.X, BanCo.QuanCoDuocChon.Quan_Co.ToaDo.Y);
+            Point destination = ProgramConfig.ThongSo.ToaDoDonViCuaDiem(((ProgramConfig.RoundButton)sender).Location); // Lấy tọa độ của RoundButton điểm bàn cờ (điểm đích)
+
+            BanCo.LoaiBoQuanCo(destination, ptbBanCo); // Loại bỏ quân cờ ở điểm đích
+            BanCo.QuanCoDuocChon.DiChuyen(destination); // Di chuyển quân cờ đến điểm đích
+
+            if (BanCo.HaiTuongDoiMatNhau()) // nước đi không hợp lệ nếu sau nước đi 2 tướng đối mặt nhau => hoàn tác nước đi
+            {
+                MessageBox.Show("Tướng phe bạn sẽ đối mặt với tướng đối phương sau nước đi này. Hãy chọn một nước đi khác.", "Nước đi không hợp lệ");
+                BanCo.QuanCoDuocChon.DiChuyen(departure);
+                BanCo.QuanCoDuocChon = null;
+                BanCo.TraLaiQuanCo(ptbBanCo, BanCo.QuanCoBiLoai);
+                BanCo.RefreshBanCo();
+                return;
+            }
+            if (BanCo.CoChieuTuong(BanCo.PheDoiPhuong())) // nước đi không hợp lệ nếu sau nước đi tướng phe di chuyển bị đối phương chiếu => hoàn tác nước đi
+            {
+                MessageBox.Show("Tướng phe bạn sẽ gặp nguy sau nước đi này. Hãy chọn một nước đi khác.", "Nước đi không hợp lệ");
+                BanCo.QuanCoDuocChon.DiChuyen(departure);
+                BanCo.QuanCoDuocChon = null;
+                BanCo.TraLaiQuanCo(ptbBanCo, BanCo.QuanCoBiLoai);
+                BanCo.RefreshBanCo();
+                return;
+            }
+            if (BanCo.CoChieuTuong(BanCo.PheDuocDanh)) // nếu sau nước đi phe di chuyển chiếu tướng phe đối phương => thông báo cho người chơi
+            {
+                if (BanCo.PheDoiPhuong() == 1)
+                    MessageBox.Show("Phe Xanh hãy đối phó với nước đi này từ phe Đỏ.", "Chiếu tướng!");
+                else
+                    MessageBox.Show("Phe Đỏ hãy đối phó với nước đi này từ phe Xanh.", "Chiếu tướng!");
+            }
+            BanCo.HienThiNuocDi(departure, destination, ptbBanCo);
+            BanCo.LuuNuocDi(departure, destination);
+            BanCo.DoiPhe(lblPheDuocDanh, lblSoLuotDi, btnNewGame, btnUndo); //*Offline*
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn muốn bỏ ván đấu này và bắt đầu một ván mới?", "Ván mới", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                //Hàm xóa bàn cờ tách thành SetToDefault + XoaBanCo
+                BanCo.SetToDefault(lblPheDuocDanh, lblSoLuotDi, btnNewGame, btnUndo);
+                BanCo.XoaBanCo(ptbBanCo);
+                BanCo.TaoDiemBanCo(ptbBanCo, DiemBanCo_Click);
+                BanCo.TaoQuanCo(QuanCo_Click, ptbBanCo);
+                BanCo.RefreshBanCo(); //*Offline*
+            }
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn muốn hoàn tác nước đi vừa rồi?", "Hoàn tác nước đi", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                btnUndo.Enabled = false;
+                BanCo.Dehighlight();
+                BanCo.AnDiemDich();
+                BanCo.HoanTac(ptbBanCo);
+                BanCo.DoiPhe(lblPheDuocDanh, lblSoLuotDi, btnNewGame);
+                BanCo.HienThiNuocDiTruoc(ptbBanCo);
+            }
+        }
+
+        private void ptrHelp_Click(object sender, EventArgs e)
+        {
+            LuatChoi.Form1 luatChoi = new LuatChoi.Form1();
+            luatChoi.Show();
+        }
+
+        private void ptrSound_Click(object sender, EventArgs e)
+        {
+            if (sound)
+            {
+
+                ptrSound.Image = Properties.Resources.SoundOff;
+                player.Stop();
+            }
+            else
+            {
+                ptrSound.Image = Properties.Resources.SoundOn;
+                PlayMusic();
+            }
+            sound = !sound;
+        }
+        private void TakeAPicture()
+        {
+            // Chụp ảnh
+            screenBitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                            Screen.PrimaryScreen.Bounds.Height,
+                                            PixelFormat.Format32bppArgb);
+            Thread.Sleep(500);
+            screenGraphics = Graphics.FromImage(screenBitmap);
+            screenGraphics.CopyFromScreen(this.Location.X, this.Location.Y,
+                                    0, 0, this.Size, CopyPixelOperation.SourceCopy);
+        }
+
+        private void SavePicture()
+        {
+            // Lưu ảnh đã chụp
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "PNG Files (.png)|*.png|All Files (*.*)|*.*";
+            saveDialog.FilterIndex = 1;
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                screenBitmap.Save(saveDialog.FileName, ImageFormat.Png);
+                MessageBox.Show("Đã lưu ảnh '" + saveDialog.FileName + "' !!", "Thành công");
+            }
+        }
+        private void ptrCamera_Click(object sender, EventArgs e)
+        {
+            TakeAPicture();
+            SavePicture();
+        }
+    }
+}
---
+++ KhoiDong/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace KhoiDong
+{
+    partial class KhoiDong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KhoiDong));
+            this.radioOffline = new System.Windows.Forms.RadioButton();
+            this.radioLAN = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPlayerName = new System.Windows.Forms.TextBox();
+            this.radioConnectPvp = new System.Windows.Forms.RadioButton();
+            this.radioNewGamePvp = new System.Windows.Forms.RadioButton();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioOffline
+            // 
+            this.radioOffline.AutoSize = true;
+            this.radioOffline.Location = new System.Drawing.Point(27, 20);
+            this.radioOffline.Name = "radioOffline";
+            this.radioOffline.Size = new System.Drawing.Size(61, 17);
+            this.radioOffline.TabIndex = 0;
+            this.radioOffline.TabStop = true;
+            this.radioOffline.Text = "Offline";
+            this.radioOffline.UseVisualStyleBackColor = true;
+            // 
+            // radioLAN
+            // 
+            this.radioLAN.AutoSize = true;
+            this.radioLAN.Location = new System.Drawing.Point(27, 48);
+            this.radioLAN.Name = "radioLAN";
+            this.radioLAN.Size = new System.Drawing.Size(45, 17);
+            this.radioLAN.TabIndex = 1;
+            this.radioLAN.TabStop = true;
+            this.radioLAN.Text = "LAN";
+            this.radioLAN.UseVisualStyleBackColor = true;
+            this.radioLAN.CheckedChanged += new System.EventHandler(this.radioLAN_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.tbPlayerName);
+            this.panel2.Controls.Add(this.radioConnectPvp);
+            this.panel2.Controls.Add(this.radioNewGamePvp);
+            this.panel2.Enabled = false;
+            this.panel2.Location = new System.Drawing.Point(27, 83);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(321, 122);
+            this.panel2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tên người chơi:";
+            // 
+            // tbPlayerName
+            // 
+            this.tbPlayerName.Location = new System.Drawing.Point(111, 17);
+            this.tbPlayerName.MaxLength = 12;
+            this.tbPlayerName.Name = "tbPlayerName";
+            this.tbPlayerName.Size = new System.Drawing.Size(142, 22);
+            this.tbPlayerName.TabIndex = 2;
+            // 
+            // radioConnectPvp
+            // 
+            this.radioConnectPvp.AutoSize = true;
+            this.radioConnectPvp.Location = new System.Drawing.Point(29, 80);
+            this.radioConnectPvp.Name = "radioConnectPvp";
+            this.radioConnectPvp.Size = new System.Drawing.Size(267, 17);
+            this.radioConnectPvp.TabIndex = 1;
+            this.radioConnectPvp.Text = "Kết nối vào một ván cờ tạo bởi người chơi khác";
+            this.radioConnectPvp.UseVisualStyleBackColor = true;
+            // 
+            // radioNewGamePvp
+            // 
+            this.radioNewGamePvp.AutoSize = true;
+            this.radioNewGamePvp.Checked = true;
+            this.radioNewGamePvp.Location = new System.Drawing.Point(29, 50);
+            this.radioNewGamePvp.Name = "radioNewGamePvp";
+            this.radioNewGamePvp.Size = new System.Drawing.Size(86, 17);
+            this.radioNewGamePvp.TabIndex = 0;
+            this.radioNewGamePvp.TabStop = true;
+            this.radioNewGamePvp.Text = "Tạo ván mới";
+            this.radioNewGamePvp.UseVisualStyleBackColor = true;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Image = global::KhoiDong.Properties.Resources.red_play_512;
+            this.btnGo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGo.Location = new System.Drawing.Point(151, 223);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(86, 27);
+            this.btnGo.TabIndex = 10;
+            this.btnGo.Text = "Bắt đầu";
+            this.btnGo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // KhoiDong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 269);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.radioLAN);
+            this.Controls.Add(this.radioOffline);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "KhoiDong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game Cờ Tướng";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioLAN;
+        private System.Windows.Forms.RadioButton radioOffline;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPlayerName;
+        private System.Windows.Forms.RadioButton radioConnectPvp;
+        private System.Windows.Forms.RadioButton radioNewGamePvp;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
